10c48bd2e3baa192afaa4698669af229c202c9b3
walker-l <2074083276@qq.com>
2018-06-03 06:57:35 +0800

    模拟秒杀抢购
    模拟秒杀抢购功能

---
+++ src/main/java/cn/walkerl/service/SecKillService.java
@@ -0,0 +1,10 @@
+package cn.walkerl.service;
+
+public interface SecKillService {
+
+	
+	public String querySecKillProductInfo(String productId);
+	
+	public void orderProductMockDiffUser(String productId);
+	
+}
